9916713a689fe9f371288d29f0a6c22de4cf03f0
loayhajjo <louy0013@edu.zealand.dk>
2022-11-02 11:28:49 +0100

    nav1.1

--- includes/nav.php
+++ includes/nav.php
@@ -2,35 +2,35 @@
     <div class="container-fluid col-12">
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
         </button>
-        <div class="collapse navbar-collapse justify-content-between " id="navbarTogglerDemo01">
-            <ul>
+        <div class="collapse navbar-collapse justify-content-between col-12 " id="navbarTogglerDemo01">
+            <ul class="col-2">
             <li class="nav-item list-unstyled">
                 <a class="nav-link active">
-                    <img class="img-fluid w-50" src="images/LoFa-logo_overunder-black_1.png">
+                    <img class="img-fluid w-50" src="images/LoFa-logo_overunder-black.svg">
                 </a>
             </li>
             </ul>
 
-            <div>
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <div class="col-8">
+            <ul class="navbar-nav  mb-2 mb-lg-0 justify-content-center">
 
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active text-dark pe-5" aria-current="page" href="#">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Omkring os</a>
+                    <a class="nav-link active text-dark pe-5" aria-current="page" href="#">Omkring os</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Hvad tilbyder vi?</a>
+                    <a class="nav-link text-dark pe-5" href="#">Hvad tilbyder vi?</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Vores team</a>
+                    <a class="nav-link text-dark pe-5" href="#">Vores team</a>
                 </li>
             </ul>
         </div>
-        <div>
-            <form class="d-flex ">
-                <button class="btn btn-outline-success" type="submit">Kontakt os</button>
+        <div class="col-2">
+            <form class="d-flex justify-content-center ">
+                <button class="btn btn-primary text-light" type="submit">Kontakt os</button>
             </form>
         </div>
         </div>
