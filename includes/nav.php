71325ebf2c829b09f28e3bfca6a39e1cede26140
loayhajjo <louy0013@edu.zealand.dk>
2022-11-02 12:31:27 +0100

    nav1.2

--- includes/nav.php
+++ includes/nav.php
@@ -1,34 +1,44 @@
-<nav class="navbar navbar-expand-lg bg-body-bg">
+<nav class="navbar navbar-expand-lg  navbar-light" style="background-color:#FAFAFA">
     <div class="container-fluid col-12">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <ul class="col-4 d-xl-none">
+            <li class="nav-item list-unstyled">
+                <a class="nav-link active p-2">
+                    <img class="img-fluid  w-100" src="images/LoFa-logo_overunder-black.svg">
+                </a>
+            </li>
+        </ul>
+
+        <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
         </button>
+
         <div class="collapse navbar-collapse justify-content-between col-12 " id="navbarTogglerDemo01">
             <ul class="col-2">
-            <li class="nav-item list-unstyled">
+            <li class="nav-item list-unstyled d-xl-block d-none">
                 <a class="nav-link active">
                     <img class="img-fluid w-50" src="images/LoFa-logo_overunder-black.svg">
                 </a>
             </li>
             </ul>
 
-            <div class="col-8">
+            <div class="col-md-8">
             <ul class="navbar-nav  mb-2 mb-lg-0 justify-content-center">
 
                 <li class="nav-item">
-                    <a class="nav-link active text-dark pe-5" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active text-dark text-center pe-md-5" aria-current="page" href="#">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active text-dark pe-5" aria-current="page" href="#">Omkring os</a>
+                    <a class="nav-link active text-dark text-center pe-md-5" aria-current="page" href="#">Omkring os</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark pe-5" href="#">Hvad tilbyder vi?</a>
+                    <a class="nav-link text-dark text-center pe-md-5" href="#">Hvad tilbyder vi?</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-dark pe-5" href="#">Vores team</a>
+                    <a class="nav-link text-dark text-center pe-md-5" href="#">Vores team</a>
                 </li>
             </ul>
         </div>
-        <div class="col-2">
+        <div class="col-md-2">
             <form class="d-flex justify-content-center ">
                 <button class="btn btn-primary text-light" type="submit">Kontakt os</button>
             </form>
