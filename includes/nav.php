5e421cf7832ee45203787638a7f1e56d844deb7d
loayhajjo <louy0013@edu.zealand.dk>
2022-11-02 10:47:38 +0100

    nav1.0

---
+++ includes/nav.php
@@ -0,0 +1,38 @@
+<nav class="navbar navbar-expand-lg bg-body-bg">
+    <div class="container-fluid col-12">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        </button>
+        <div class="collapse navbar-collapse justify-content-between " id="navbarTogglerDemo01">
+            <ul>
+            <li class="nav-item list-unstyled">
+                <a class="nav-link active">
+                    <img class="img-fluid w-50" src="images/LoFa-logo_overunder-black_1.png">
+                </a>
+            </li>
+            </ul>
+
+            <div>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Omkring os</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Hvad tilbyder vi?</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Vores team</a>
+                </li>
+            </ul>
+        </div>
+        <div>
+            <form class="d-flex ">
+                <button class="btn btn-outline-success" type="submit">Kontakt os</button>
+            </form>
+        </div>
+        </div>
+    </div>
+</nav>
