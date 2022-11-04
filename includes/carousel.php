bbe2aa5f1fdc1def0d2a9411210964805be9a6c7
MikkelLebedev <mikkel.lebedev@outlook.com>
2022-11-04 14:00:12 +0100

    Hero v3

---
+++ includes/carousel.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<!-- Coding by CodingLab | www.codinglabweb.com -->
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Responsive Card Slider</title>
+
+	<!-- Swiper CSS -->
+	<link rel="stylesheet" href="css/swiper-bundle.min.css">
+
+	<!-- CSS -->
+	<link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+<div class="slide-container swiper">
+	<div class="slide-content">
+		<div class="card-wrapper swiper-wrapper">
+			<div class="card swiper-slide">
+				<div class="image-content">
+					<span class="overlay"></span>
+
+					<div class="card-image">
+						<img src="images/BE5A0023-Edit-2.JPG" alt="" class="card-img">
+					</div>
+				</div>
+
+				<div class="card-content">
+					<h2 class="name">Tommy Nguyen</h2>
+					<p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
+
+					<button class="button">View More</button>
+				</div>
+			</div>
+			<div class="card swiper-slide">
+				<div class="image-content">
+					<span class="overlay"></span>
+
+					<div class="card-image">
+						<img src="images/BE5A4981.JPG" alt="" class="card-img">
+					</div>
+				</div>
+
+				<div class="card-content">
+					<h2 class="name">Loay Brooklyn Hajjo</h2>
+					<p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
+
+					<button class="button">View More</button>
+				</div>
+			</div>
+			<div class="card swiper-slide">
+				<div class="image-content">
+					<span class="overlay"></span>
+
+					<div class="card-image">
+						<img src="images/BE5A5859-Edit.JPG" alt="" class="card-img">
+					</div>
+				</div>
+
+				<div class="card-content">
+					<h2 class="name">Mikkel Lebedev Johnsen</h2>
+					<p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
+
+					<button class="button">View More</button>
+				</div>
+			</div>
+			<div class="card swiper-slide">
+				<div class="image-content">
+					<span class="overlay"></span>
+
+					<div class="card-image">
+						<img src="images/BE5A5878.JPG" alt="" class="card-img">
+					</div>
+				</div>
+
+				<div class="card-content">
+					<h2 class="name">Amanda Keis</h2>
+					<p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
+
+					<button class="button">View More</button>
+				</div>
+			</div>
+			<div class="card swiper-slide">
+				<div class="image-content">
+					<span class="overlay"></span>
+
+					<div class="card-image">
+						<img src="images/BE5A5881.JPG" alt="" class="card-img">
+					</div>
+				</div>
+
+				<div class="card-content">
+					<h2 class="name">Kevin Pedersen</h2>
+					<p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
+
+					<button class="button">View More</button>
+				</div>
+			</div>
+			<div class="card swiper-slide">
+				<div class="image-content">
+					<span class="overlay"></span>
+
+					<div class="card-image">
+						<img src="images/BE5A5892-Edit.JPG" alt="" class="card-img">
+					</div>
+				</div>
+
+				<div class="card-content">
+					<h2 class="name">Sidney Goojvin</h2>
+					<p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
+
+					<button class="button">View More</button>
+				</div>
+			</div>
+			<div class="card swiper-slide">
+				<div class="image-content">
+					<span class="overlay"></span>
+
+					<div class="card-image">
+						<img src="images/BE5A5846-Edit.JPG" alt="" class="card-img">
+					</div>
+				</div>
+
+				<div class="card-content">
+					<h2 class="name">Nikita Wulff-Høyer</h2>
+					<p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
+
+					<button class="button">View More</button>
+				</div>
+			</div>
+			<div class="card swiper-slide">
+				<div class="image-content">
+					<span class="overlay"></span>
+
+					<div class="card-image">
+						<img src="images/BE5A5846-Edit.JPG" alt="" class="card-img">
+					</div>
+				</div>
+
+				<div class="card-content">
+					<h2 class="name">Freja A.H Rose</h2>
+					<p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
+
+					<button class="button">View More</button>
+				</div>
+			</div>
+			<div class="card swiper-slide">
+				<div class="image-content">
+					<span class="overlay"></span>
+
+					<div class="card-image">
+						<img src="images/BE5A5892-Edit.JPG" alt="" class="card-img">
+					</div>
+				</div>
+
+				<div class="card-content">
+					<h2 class="name">Sita Shemesh</h2>
+					<p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
+
+					<button class="button">View More</button>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="swiper-button-next swiper-navBtn"></div>
+	<div class="swiper-button-prev swiper-navBtn"></div>
+	<div class="swiper-pagination"></div>
+</div>
+
+</body>
+
+<!-- Swiper JS -->
+<script src="js/swiper-bundle.min.js"></script>
+
+<!-- JavaScript -->
+<script src="js/script.js"></script>
+
+</html>
